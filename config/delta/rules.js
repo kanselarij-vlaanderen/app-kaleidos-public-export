d072f1716321692fb4573dffff4015e66beaf5fd
Erika Pauwels <erika.pauwels@gmail.com>
2020-06-11 21:33:42 +0200

    Update delta rule for ttl-to-delta service

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,13 +1,20 @@
 export default [
   {
     match: {
-      object: { type: "uri", value: "http://mu.semte.ch/vocabularies/ext/TtlToDeltaTask" }
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/ns/adms#status'
+      },
+      object: {
+        type: 'uri',
+        value: 'http://redpencil.data.gift/ttl-to-delta-tasks/8C7E9155-B467-49A4-B047-7764FE5401F7'
+      }
     },
     callback: {
-      url: "http://ttl-to-delta/new-ttl", method: "POST"
+      url: 'http://ttl-to-delta/delta', method: 'POST'
     },
     options: {
-      resourceFormat: "v0.0.1",
+      resourceFormat: 'v0.0.1',
       gracePeriod: 1000,
       ignoreFromSelf: true
     }
