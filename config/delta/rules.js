dcef52127c4a83874137e3169e0516c9cbd07374
Erika Pauwels <erika.pauwels@gmail.com>
2020-08-04 15:00:06 +0200

    Add services to automate Valvas publication via delta syncs

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -19,4 +19,4 @@ export default [
       ignoreFromSelf: true
     }
   }
-]
+];
--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -34,6 +34,10 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://file/files/"
   end
 
+  get "/kaleidos-files/*path" do
+    Proxy.forward conn, path, "http://kaleidos-public-file/files/"
+  end
+
   match _ do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
